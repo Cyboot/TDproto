8894757f0cd346c1ac2b17317b0fa520145a3c3c
DoubleSteakhouse <double.steakhouse@gmail.com>
2013-10-02 18:10:28 +0200

    added Player with coins and health

--- src/net/tmt/td/entity/GenericMinion.java
+++ src/net/tmt/td/entity/GenericMinion.java
@@ -1,5 +1,6 @@
 package net.tmt.td.entity;
 
+import net.tmt.td.game.Player;
 import net.tmt.td.util.Vector2d;
 
 public abstract class GenericMinion extends Entity {
@@ -11,6 +12,7 @@ public abstract class GenericMinion extends Entity {
 	protected boolean		isAlive;
 	protected int			health;
 	protected int			type;
+	protected int			damage;
 	protected float			armor;
 	protected Vector2d		speedVector;
 	protected Vector2d		directionVector;
@@ -20,8 +22,9 @@ public abstract class GenericMinion extends Entity {
 		isAlive = true;
 		health = 100;
 		type = TYPE_NOONE;
+		damage = 1;
 		armor = 0.0f;
-		speedVector = new Vector2d(1d, 2d);// just for example
+		speedVector = new Vector2d(3d, 3d);// just for example
 		// set from spawner, which knows the first directionShield from specific
 		// map
 		directionVector = new Vector2d(1, 0);
@@ -33,6 +36,18 @@ public abstract class GenericMinion extends Entity {
 
 	public Vector2d getDirection() {
 		return directionVector;
+	}
+
+	public void attackPlayer() {
+		Player.getInstance().changeHealthValueTo(damage * -1);
+	}
+
+	public void onKilled() {
+		isAlive = false;
+	}
+
+	public boolean isAlive() {
+		return isAlive;
 	}
 
 }
